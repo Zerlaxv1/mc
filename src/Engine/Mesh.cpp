f71bd108e20d993f25f53426bbf6264cc103101d
Zerlax_ <nino.duduch@gmail.com>
2024-09-19 20:18:47 +0200

    clean

--- src/Engine/Mesh.cpp
+++ src/Engine/Mesh.cpp
@@ -17,60 +17,6 @@ void Mesh::setVariables(float *vertices, unsigned int *indices, unsigned int ver
 }
 
 void Mesh::setupMesh() {
-    // unsigned int indicesArray[6] = {
-    //     0, 1, 3, // first triangle
-    //     1, 2, 3 // second triangle
-    // };
-    //
-    // float verticesArray[180] = {
-    //     // positions          // texture coords
-    //     -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-    //     0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-    //     0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-    //     0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-    //     -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-    //     -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-    //
-    //     -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-    //     0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-    //     0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-    //     0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-    //     -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-    //     -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-    //
-    //     -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-    //     -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-    //     -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-    //     -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-    //     -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-    //     -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-    //
-    //     0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-    //     0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-    //     0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-    //     0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-    //     0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-    //     0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-    //
-    //     -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-    //     0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-    //     0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-    //     0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-    //     -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-    //     -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-    //
-    //     -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-    //     0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-    //     0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-    //     0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-    //     -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-    //     -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
-    // };
-    //
-    // this->verticesCount = 180;
-    // this->verticesSize = 180 * sizeof(float);
-    // this->indicesCount = 6;
-    // this->indicesSize = 6 * sizeof(unsigned int);
 
     glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STATIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesSize, indices, GL_STATIC_DRAW);
