f5fcdbef86bcaa1d7110e3478fe800c32efae7e5
Zerlax_ <nino.duduch@gmail.com>
2025-02-17 00:20:29 +0100

    jsp

--- src/Game/World.cpp
+++ src/Game/World.cpp
@@ -5,7 +5,7 @@
 #include "World.h"
 
 // return the chunk at the given position
-Chunk* World::getChunk(int x, int y, int z) {
+Chunk *World::getChunk(int x, int y, int z) {
     auto it = chunks.find(std::make_tuple(x, y, z));
     if (it != chunks.end()) {
         return &it->second;
@@ -15,7 +15,7 @@ Chunk* World::getChunk(int x, int y, int z) {
 
 // activate the block at the given position in the given chunk
 void World::activateBlock(int chunkX, int chunkY, int chunkZ, int voxelX, int voxelY, int voxelZ) {
-    Chunk* chunk = getChunk(chunkX, chunkY, chunkZ);
+    Chunk *chunk = getChunk(chunkX, chunkY, chunkZ);
     chunk->activateBlock(voxelX, voxelY, voxelZ);
 }
 
@@ -25,7 +25,7 @@ void World::generateFlatWorld() {
     for (int x = -1; x <= 1; x++) {
         for (int y = -1; y <= 1; y++) {
             for (int z = -1; z <= 1; z++) {
-                Chunk& chunk = chunks[std::make_tuple(x, y, z)];
+                Chunk &chunk = chunks[std::make_tuple(x, y, z)];
                 chunk.generateFlatChunk(x, y, z);
             }
         }
@@ -57,8 +57,16 @@ void World::combineChunkMeshes() {
     // array of all the meshes
     std::vector<Mesh> chunkMeshes;
 
-    for (auto& [pos, chunk] : chunks) {
-        Mesh* mesh = chunk.getChunkMesh();
+    for (auto &[pos, chunk]: chunks) {
+        Mesh *mesh = chunk.getChunkMesh();
+
+        std::cout << "Mesh of chunk (" << std::get<0>(pos) << ", " << std::get<1>(pos) << ", " << std::get<2>(pos) <<
+                ")"
+                << " has " << mesh->getVertices().size() / 6
+                << " vertices and "
+                << mesh->getIndices().size()
+                << " indices" << std::endl;
+
         if (mesh != nullptr) {
             chunkMeshes.push_back(*mesh);
         }
@@ -67,13 +75,15 @@ void World::combineChunkMeshes() {
     combinedMesh = Mesh::CombineMeshes(chunkMeshes);
     // print vertices and indices
     for (int i = 0; i < 200; i += 6) {
-        std::cout << "Vertex " << i / 6 << " : " << combinedMesh.getVertices()[i] << " " << combinedMesh.getVertices()[i + 1] << " " << combinedMesh.getVertices()[i + 2] << std::endl;
+        std::cout << "Vertex " << i / 6 << " : X = " << combinedMesh.getVertices()[i] << ", Y = " << combinedMesh.
+                getVertices()[i + 1] << ", Z = " << combinedMesh.getVertices()[i + 2] << std::endl;
     }
     // print the number of vertices and indices in the combined mesh
-    std::cout << "Combined mesh has " << combinedMesh.getVertices().size() / 6 << " vertices and " << combinedMesh.getIndices().size() << " indices" << std::endl;
+    std::cout << "Combined mesh has " << combinedMesh.getVertices().size() / 6 << " vertices and " << combinedMesh.
+            getIndices().size() << " indices" << std::endl;
 
     // print coos of all chunk created
-    for (auto& [pos, chunk] : chunks) {
+    for (auto &[pos, chunk]: chunks) {
         std::cout << "Chunk at " << std::get<0>(pos) << " " << std::get<1>(pos) << " " << std::get<2>(pos) << std::endl;
     }
 }
@@ -83,11 +93,10 @@ void World::toggleCameraLock() {
 }
 
 // constructor
-World::World() :
-renderer(&combinedMesh, &shader, &camera, &texture),
-shader("./Resources/Shaders/VertexTextures.glsl", "./Resources/Shaders/fragmentTextures.glsl"),
-camera(glm::vec3(0.0f, 128.0f, 0.0f))
-{}
+World::World() : renderer(&combinedMesh, &shader, &camera, &texture),
+                 shader("./Resources/Shaders/VertexTextures.glsl", "./Resources/Shaders/fragmentTextures.glsl"),
+                 camera(glm::vec3(0.0f, 128.0f, 0.0f)) {
+}
 
 void World::Init() {
     generateFlatWorld();
